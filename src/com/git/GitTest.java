0bbf5a63f8d3ee2a9e2bbe4190bd540a706f8f1b
GaoChengQuan <529390053@qq.com>
2017-08-30 10:38:22 +0800

    commit1

--- src/com/git/GitTest.java
+++ src/com/git/GitTest.java
@@ -3,9 +3,5 @@ package com.git;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("GitTest.main()");
-		System.out.println("GitTest.main()");
-		System.out.println("GitTest.main()");
-		System.out.println("GitTest.main()");
-		System.out.println("GitTest.main()");
 	}
 }
