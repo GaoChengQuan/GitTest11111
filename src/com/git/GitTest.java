13501c3fc1dccdae7861036d944cefec163f5d50
GaoChengQuan <529390053@qq.com>
2017-08-30 10:36:46 +0800

    init

---
+++ src/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("GitTest.main()");
+		System.out.println("GitTest.main()");
+		System.out.println("GitTest.main()");
+		System.out.println("GitTest.main()");
+		System.out.println("GitTest.main()");
+	}
+}
